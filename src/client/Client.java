0a3335571dd4e5ecb2785cc693bca55e5f8f4565
nordinezerai <nordine.zerai@gmail.com>
2017-11-09 11:26:10 +0100

    Initial commit

---
+++ src/client/Client.java
@@ -0,0 +1,15 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String argv[]) throws Exception
+    {
+
+    }
+
+}
---
+++ src/serveur/Serveur.java
@@ -0,0 +1,15 @@
+package serveur;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Serveur {
+
+    public static void main(String argv[]) throws Exception
+    {
+
+    }
+
+}
